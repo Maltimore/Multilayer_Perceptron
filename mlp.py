a57adb718a752644f94ef2af4a6f0693eb6e4f3b
Maltimore <malte.esders@gmail.com>
2016-01-24 12:38:13 +0100

    arbitrary number of hidden layers works now

--- mlp.py
+++ mlp.py
@@ -8,23 +8,27 @@ def sigmoid(x):
 def deriv_sigmoid(x):
     return np.multiply(x, (1-x))
 
-def construct_network(n_out, n_hidden=6):
-    W1 = np.matrix(np.random.normal(size=(n_hidden, x.shape[0]+1)))
-    W2 = np.matrix(np.random.normal(size=(n_out, n_hidden+1)))
-    return W1, W2
+def construct_network(layer_sizes):
+    weights = []
+    for idx in range(len(layer_sizes)-1):
+        weights.append(np.matrix(np.random.normal(size=(layer_sizes[idx+1],layer_sizes[idx]+1))))
+    return weights
 
-def forward_propagate(x, W1, W2):
-    hidden = sigmoid(W1 @ x)
-    hidden = np.vstack((hidden, np.matrix([1]))) # adding the bias
-    output = W2 @ hidden
-    return hidden, output
+def forward_propagate(x, weights):
+    layer_activations = [np.vstack((x, np.matrix([1])))]
+    for idx in range(len(weights)-1):
+        W = weights[idx]
+        layer_activations.append(sigmoid(W @ layer_activations[idx]))
+        layer_activations[idx+1] = np.vstack((layer_activations[idx+1], np.matrix([1]))) # adding the bias
+    output = weights[-1] @ layer_activations[-1]
+    layer_activations.append(output)    
+    return layer_activations, output
 
-def classify(X, W1, W2):
+def classify(X, weights):
     Y_hat= np.matrix(np.empty((n_data)))
     for data_idx, x in enumerate(X.T):
         x = X[:, data_idx]
-        y = Y[0, data_idx]
-        _, output = forward_propagate(x, W1, W2)
+        _, output = forward_propagate(x, weights)
         Y_hat[:, data_idx] = output
     return Y_hat
 
@@ -33,53 +37,55 @@ def compute_error(Y, Y_hat, n):
 
 # create sample data
 n_data = 100
-n_loops = 10000
+n_loops = 200
 x_dimension = 1
-n_hidden = 20
 X = np.matrix((np.random.normal(size=(x_dimension, n_data))))
-X = np.vstack((X, np.matrix(np.ones(X.shape[1]))))
 Y = np.matrix(np.empty(X.shape[1]))
+
+#def train(X, Y, hidden_layer_sizes, error_deriv):
+hidden_layer_sizes = [30, 30] # DELETE LATER
+layer_sizes = [X.shape[0]] + hidden_layer_sizes + [Y.shape[0]]
 for idx, x in enumerate(X.T):
     Y[0,idx] = x[0,0]**2 # + np.random.normal(scale=.5)
-W1, W2 = construct_network(Y.shape[0], n_hidden)
-
-Y_hat = classify(X, W1, W2)
-error = compute_error(Y, Y_hat, n_data)
-plt.figure()
-plt.plot(X[0,:].T, Y[0,:].T, "x", label="true")
-plt.plot(X[0,:].T, Y_hat[0,:].T, "x", label="predicted")
-plt.legend()
-plt.title("Error is: " + str(error))
+weights = construct_network(layer_sizes)
 
 errorvec = np.empty((n_loops))
 for loop in np.arange(n_loops):
-    delta_W1 = np.zeros(W1.shape)
-    delta_W2 = np.zeros(W2.shape)
+    delta_W = []
+    # initiazlizing delta Ws
+    for weight_mat_idx in range(len(weights)):
+        delta_W.append(np.zeros((weights[weight_mat_idx].shape)))
+        
     for data_idx in np.arange(n_data):
         x = X[:, data_idx]
         y = Y[:, data_idx]
-        hidden, output = forward_propagate(x, W1, W2)
+        layer_activations, output = forward_propagate(x, weights)
         
-        delta_output = output - y
-        #print("delta_output is: " + str(delta_output))
-        delta_hidden = -np.multiply(deriv_sigmoid(hidden), W2.T @ delta_output)
-        delta_hidden = delta_hidden[:-1,0] # removing bias
-        delta_W2 -= delta_output @ hidden.T
-        delta_W2[:,:-1] = np.mean(delta_hidden)
-        delta_W1 -= delta_hidden @ x.T
-        delta_W1[:,:-1] = np.mean(x[:-1,0])
-
-    W1 += 1/n_data * delta_W1
-    W2 += 1/n_data * delta_W2
+        errors = [output - y]
+        # go from the output layer towards the input layer and calculate error values
+        for idx in np.arange(1, len(weights)):
+            # prepend the newest error layer to the errors list:
+            errors = [-np.multiply(deriv_sigmoid(layer_activations[-idx-1]),  \
+                                   weights[-idx].T @ errors[-idx])]             \
+                     + errors
+            errors[-idx-1] = errors[-idx-1][:-1,0] # removing bias
+        # go from input layer towards output layer and calculate weight updates
+        for idx in range(len(weights)):
+            delta_W[idx] -= errors[idx] @ layer_activations[idx].T
+            delta_W[idx][:,:-1] = np.mean(layer_activations[idx])
     
-    Y_hat = classify(X, W1, W2)
+    # update all weight matrices
+    for idx in range(len(weights)):    
+        weights[idx] += 1/n_data * delta_W[idx]
+    
+    Y_hat = classify(X, weights)
     error = compute_error(Y, Y_hat, n_data)
     errorvec[loop] = error
 
 plt.figure()
 plt.plot(np.arange(n_loops), errorvec)
 
-Y_hat = classify(X, W1, W2)
+Y_hat = classify(X, weights)
 plt.figure()
 plt.plot(X[0,:].T, Y[0,:].T, "x", label="true")
 plt.plot(X[0,:].T, Y_hat[0,:].T, "x", label="predicted")
